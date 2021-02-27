244c6ebb6ae6a489b2b2488be1d15b9bee27c3cb
sidharsha <siddharth.landers@gmail.com>
2021-02-27 02:47:40 -0500

    Added a navbar for the blog

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from './Navbar';
+import Home from './Home';
+// The function name must always start with a capital letter.
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  // const title = "Harsha's Personal Blog";
+  // const likes = 50;
+  // const person = {
+  //   name: 'yoshi',
+  //   age:30
+  // };
+  // const link = "http://www.google.com"
 
-export default App;
+  // return (
+  //   <div className="App">
+  //     <div className="content">
+  //       <h1>{title}</h1>
+  //       <p>Liked {likes} times</p>
+
+  //       <p>{10}</p>
+  //       <p>{"hello ppl"}</p>
+  //       <p>{Math.random()*10}</p>
+
+  //       <a href={link}>google</a>
+  //     </div>
+  //   </div>
+  // );
+  return(
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  )
+}
+ 
+export default App; // We always export so that we can use it in other files
