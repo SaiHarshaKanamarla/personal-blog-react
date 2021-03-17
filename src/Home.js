fe38564ca3fbbdd6e7b48d746c069090d6dd558f
sidharsha <siddharth.landers@gmail.com>
2021-03-17 14:10:22 -0400

    Creating a custom hook for fetch data from REST API endpoints

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,13 @@
 import {useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
+
 
 const Home = () => {
 
     const [name, setName] = useState('harsha'); //useState hook
-    const [age, setAge] = useState(25)
-    const [blogs,setBlogs] = useState(null);
-    const [isPending, setisPending] = useState(true);
-    const [error, setError] = useState(null);
-;    
+    const [age, setAge] = useState(25);
+       
     const [nameo,setNameo] = useState('harsha');
     const handleClick = (e) => {             
         setName('Siddu');
@@ -24,31 +23,34 @@ const Home = () => {
     //     setBlogs(newBlogs);
     // }
 
-    useEffect(() => {
-        // This is a way to run code for every render(gets fired on every render). We dont store it in a constatnt cuz it does not return anything
-        // We can do tasks like fetching data or authenticating services.
-        // Beware of changing the state inside a useEffect. This is cause a cascading loop
-        fetch('http://localhost:8000/blogs')
-            .then((res)=>{
-                console.log(res);
-                if(!res.ok){
-                    // response is not okay. SO we throw an error.
-                    throw Error('Could not fetch data from the resource'); // When we throw this error, it is caught in the .catch block
-                }
-                return res.json();
-            })
-            .then((data)=>{
-                console.log(data);
-                setBlogs(data);
-                setError(null);
-                setisPending(false);
-            })
-            .catch((err)=>{
-                setisPending(false);
-                setError(err.message);
-            })
-    },[]); // The [] denotes depenedencies for useEffect(). If it's empty, it only runs useEffect just once(ie the initial render). If the state changes later, it won't run it again
+    // useEffect(() => {
+    //     // This is a way to run code for every render(gets fired on every render). We dont store it in a constatnt cuz it does not return anything
+    //     // We can do tasks like fetching data or authenticating services.
+    //     // Beware of changing the state inside a useEffect. This is cause a cascading loop
+    //     fetch('http://localhost:8000/blogs')
+    //         .then((res)=>{
+    //             console.log(res);
+    //             if(!res.ok){
+    //                 // response is not okay. SO we throw an error.
+    //                 throw Error('Could not fetch data from the resource'); // When we throw this error, it is caught in the .catch block
+    //             }
+    //             return res.json();
+    //         })
+    //         .then((data)=>{
+    //             console.log(data);
+    //             setBlogs(data);
+    //             setError(null);
+    //             setisPending(false);
+    //         })
+    //         .catch((err)=>{
+    //             setisPending(false);
+    //             setError(err.message);
+    //         })
+    // },[]); // The [] denotes depenedencies for useEffect(). If it's empty, it only runs useEffect just once(ie the initial render). If the state changes later, it won't run it again
     // the [] brackets are used to name the dependencies for the useEffect(). So basically, the useEffect looks for changes in the state of name. If the name changes, then useEffect is invoked.
+
+    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
+
     return (
         <div className="home">
             {/* <h2>Homepage</h2>
---
+++ src/useFetch.js
@@ -0,0 +1,36 @@
+import {useState, useEffect } from 'react';
+
+
+const useFetch = (url) => {
+    const [data,setData] = useState(null);
+    const [isPending, setisPending] = useState(true);
+    const [error, setError] = useState(null);
+    useEffect(() => {
+        // This is a way to run code for every render(gets fired on every render). We dont store it in a constatnt cuz it does not return anything
+        // We can do tasks like fetching data or authenticating services.
+        // Beware of changing the state inside a useEffect. This is cause a cascading loop
+        fetch(url)
+            .then((res)=>{
+                console.log(res);
+                if(!res.ok){
+                    // response is not okay. SO we throw an error.
+                    throw Error('Could not fetch data from the resource'); // When we throw this error, it is caught in the .catch block
+                }
+                return res.json();
+            })
+            .then((data)=>{
+                console.log(data);
+                setData(data);
+                setError(null);
+                setisPending(false);
+            })
+            .catch((err)=>{
+                setisPending(false);
+                setError(err.message);
+            })
+    },[]);
+
+    return {data, isPending, error}
+}
+
+export default useFetch;
