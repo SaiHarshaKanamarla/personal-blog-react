12a8e779915b7cd99f9ef129f9ea86a36ea2e48f
sidharsha <siddharth.landers@gmail.com>
2021-04-01 03:36:43 -0400

    Deleting the blogs and handling the 404 states

---
+++ src/NotFound.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+const NotFound = () => {
+    return ( <div className="not-found">
+        <h2>Sorry</h2>
+        <p>Page does not Exist</p>
+        <Link to="/"> Back to Homepage</Link>
+    </div> );
+}
+ 
+export default NotFound;
